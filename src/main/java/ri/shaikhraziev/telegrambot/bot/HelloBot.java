74f6b0939874f9f1d0f331822a3d9f3c58db4349
Mikhail <misha_shaikh@mail.ru>
2023-09-29 06:24:02 +0300

    create telegramBot

---
+++ src/main/java/ri/shaikhraziev/telegrambot/bot/HelloBot.java
@@ -0,0 +1,41 @@
+package ri.shaikhraziev.telegrambot.bot;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class HelloBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            String name = update.getMessage().getChat().getUserName();
+            String msg = update.getMessage().getText();
+
+            System.out.println(name + ": " + msg);
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "messageforurfubot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6649568210:AAHrEnGtbP29Hh-WoaJT4Nu_4BN7K25-o8U";
+    }
+}
